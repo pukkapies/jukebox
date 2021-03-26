4c1523f315efcf9728d15ca9c31e0702ff449bb4
pukkapies <knwebster@me.com>
2021-03-26 17:18:17 +0000

    Add external_id

--- jukebox/get_encodings.py
+++ jukebox/get_encodings.py
@@ -171,6 +171,7 @@ for client_name in mp3_dict:
 
         csv['client'].append(client_name)
         csv['media_id'].append(mp3_metadata['media_id'])
+        csv['external_id'].append(mp3_metadata['external_id'])
         csv['s3_key'].append(mp3_metadata['s3_key'])
         for k, v in _metrics.items():
             csv[k].append(float(v.squeeze().cpu().numpy()))
