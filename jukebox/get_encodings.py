26325f3c6c37b6a0512235963d9643cb39ffc620
pukkapies <knwebster@me.com>
2021-05-08 07:30:36 +0100

    Compute encodings

--- jukebox/get_encodings.py
+++ jukebox/get_encodings.py
@@ -21,6 +21,11 @@ def load_json(path):
     return j
 
 
+def save_as_json(d, path, sort_keys=True):
+    with open(path, 'w+') as json_file:
+        json.dump(d, json_file, sort_keys=sort_keys, indent=4)
+
+
 def get_bandwidth(mp3, hps):
     stft = librosa.core.stft(mp3, hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
     spec = np.absolute(stft)
@@ -81,21 +86,7 @@ def save_spec_plot(spec, path, title=None):
 
 # mp3_folder = '/srv/audio_mp3s/uploads/5f2b0f6df270d976b43cdafc'
 audio_mp3s_folder = '/srv/audio_mp3s'
-json_path = '/home/kevin/feedforward/mp3s_for_jukebox_test.json'
-mp3_dict = load_json(json_path)
 output_folder = '/home/kevin/pukkapies_github/jukebox/tests'
-# mp3_paths = [f for f in os.listdir(mp3_folder) if f.endswith(".mp3")]
-# shuffle(mp3_paths)
-# print(mp3_paths[:5])
-# filename = 's3_1596681011742_273098292.mp3'
-#for filename in mp3_paths[:5]:
-# mp3_path = os.path.join(mp3_folder, filename)
-csv = {"client": [], "media_id": [], "external_id": [], "s3_key": [], 'recons_loss_l3': [], 'spectral_loss_l3':[],
-       'multispectral_loss_l3': [], 'recons_loss_l2': [], 'spectral_loss_l2': [], 'multispectral_loss_l2': [],
-       'recons_loss_l1': [], 'spectral_loss_l1': [], 'multispectral_loss_l1': [], 'recons_loss': [],
-       'spectral_loss': [], 'multispectral_loss': [], 'spectral_convergence': [], 'l2_loss': [], 'l1_loss': [],
-       'linf_loss': [], 'commit_loss': []
-       }
 
 sample_options = {
     "name": "sample_5b",
@@ -108,8 +99,8 @@ sample_options = {
 }
 
 train_options = {
-        "bs": 1,
-        "labels": False
+    "bs": 1,
+    "labels": False
 }
 
 rank, local_rank, device = setup_dist_from_mpi(port=29500)
@@ -119,77 +110,136 @@ hps = Hyperparams(**sample_options)
 hps = setup_hparams("vqvae", dict(sample_length=hps.get('sample_length', 0),
                                   sample_length_in_seconds=hps.get('sample_length_in_seconds', 0),
                                   labels=False, bs=1))
-
+# print(hps)
 
 vqvae = make_vqvae(hps, 'cuda:0')
-print("sample_length", vqvae.sample_length)
-print('multipliers', vqvae.multipliers)
-print('x_shape', vqvae.x_shape)
-print('downsamples', vqvae.downsamples)
-print('hop lengths', vqvae.hop_lengths)
-print('z shapes', vqvae.z_shapes)
-print('levels', vqvae.levels)
-
-print(len(vqvae.encoders))
-# print(vqvae.encoders[0])
-
-forw_kwargs = dict(loss_fn=hps.loss_fn, hps=hps)
-
-# hps.ngpus = dist.get_world_size()
-hps.argv = " ".join(sys.argv)
-hps.bs_sample = hps.nworkers = hps.bs = 1
 
 
-for client_name in mp3_dict:
-    if not os.path.exists(os.path.join(output_folder, client_name)):
-        os.makedirs(os.path.join(output_folder, client_name))
-    if not os.path.exists(os.path.join(output_folder, client_name, 'audio')):
-        os.makedirs(os.path.join(output_folder, client_name, 'audio'))
-    if not os.path.exists(os.path.join(output_folder, client_name, 'spec')):
-        os.makedirs(os.path.join(output_folder, client_name, 'spec'))
+def compute_metrics(vqvae, hps, output_folder):
+    json_path = '/home/kevin/feedforward/mp3s_for_jukebox_test.json'
+    mp3_dict = load_json(json_path)
+    csv = {"client": [], "media_id": [], "external_id": [], "s3_key": [], 'recons_loss_l3': [], 'spectral_loss_l3':[],
+           'multispectral_loss_l3': [], 'recons_loss_l2': [], 'spectral_loss_l2': [], 'multispectral_loss_l2': [],
+           'recons_loss_l1': [], 'spectral_loss_l1': [], 'multispectral_loss_l1': [], 'recons_loss': [],
+           'spectral_loss': [], 'multispectral_loss': [], 'spectral_convergence': [], 'l2_loss': [], 'l1_loss': [],
+           'linf_loss': [], 'commit_loss': []
+           }
 
-    for mp3_metadata in mp3_dict[client_name]:  # 'external_id', 'media_id', 'num_samples', 's3_key'
-        print(mp3_metadata)
-        s3_key = mp3_metadata['s3_key']
-        filename = s3_key.split('/')[-1]
-        mp3_path = os.path.join(audio_mp3s_folder, s3_key)
-        mp3, _ = librosa.core.load(mp3_path, sr=44100)
-        librosa.output.write_wav("{}/{}.wav".format(os.path.join(output_folder, client_name, 'audio'),
-                                                    filename.split('.')[0]),
-                                 mp3[:881920], sr=44100)
-        # shutil.copyfile(mp3_path, os.path.join(output_folder, client_name, 'audio', mp3_path.split('/')[-1]))
+    print("sample_length", vqvae.sample_length)
+    print('multipliers', vqvae.multipliers)
+    print('x_shape', vqvae.x_shape)
+    print('downsamples', vqvae.downsamples)
+    print('hop lengths', vqvae.hop_lengths)
+    print('z shapes', vqvae.z_shapes)
+    print('levels', vqvae.levels)
 
-        hps.bandwidth = get_bandwidth(mp3, hps)
-        inputs = torch.tensor(mp3[:881920]).view(1, -1, 1).to(device)
+    print(len(vqvae.encoders))
+    # print(vqvae.encoders[0])
 
-        mp3_spec = spec(inputs.squeeze().cpu(), hps).numpy()
-        # save_spec_plot(mp3_spec, os.path.join(output_folder, client_name, 'spec', filename.split('.')[0] + '.png'),
-        #                title=filename.split('.')[0])
+    forw_kwargs = dict(loss_fn=hps.loss_fn, hps=hps)
 
-        inputs = audio_preprocess(inputs, hps)
-        x_outs, loss, _metrics = vqvae(inputs, **forw_kwargs, return_all_x_outs=True)  # x_outs with top level first
+    # hps.ngpus = dist.get_world_size()
+    hps.argv = " ".join(sys.argv)
+    hps.bs_sample = hps.nworkers = hps.bs = 1
 
-        # print("Loss: {}".format(loss))
-        # print("Metrics:", _metrics)
+    for client_name in mp3_dict:
+        if not os.path.exists(os.path.join(output_folder, client_name)):
+            os.makedirs(os.path.join(output_folder, client_name))
+        if not os.path.exists(os.path.join(output_folder, client_name, 'audio')):
+            os.makedirs(os.path.join(output_folder, client_name, 'audio'))
+        if not os.path.exists(os.path.join(output_folder, client_name, 'spec')):
+            os.makedirs(os.path.join(output_folder, client_name, 'spec'))
 
-        out_specs = []
-        for i, x_out in enumerate(reversed(x_outs)):  # level 0 (bottom) first
-            x_out_np = x_out.cpu().squeeze().numpy()
-            librosa.output.write_wav("{}/{}_recon{}.wav".format(os.path.join(output_folder, client_name, 'audio'),
-                                                              filename.split('.')[0], i),
-                                     x_out_np, sr=44100)
-            x_out_spec = spec(x_out.squeeze().cpu(), hps).numpy()
-            out_specs.append(x_out_spec)
+        for mp3_metadata in mp3_dict[client_name]:  # 'external_id', 'media_id', 'num_samples', 's3_key'
+            print(mp3_metadata)
+            s3_key = mp3_metadata['s3_key']
+            filename = s3_key.split('/')[-1]
+            mp3_path = os.path.join(audio_mp3s_folder, s3_key)
+            mp3, _ = librosa.core.load(mp3_path, sr=44100)
+            librosa.output.write_wav("{}/{}.wav".format(os.path.join(output_folder, client_name, 'audio'),
+                                                        filename.split('.')[0]),
+                                     mp3[:881920], sr=44100)
 
-        save_spec_plot([mp3_spec] + out_specs, os.path.join(output_folder, client_name, 'spec', filename.split('.')[0] + '.png'),
-                       title=filename.split('.')[0])
+            hps.bandwidth = get_bandwidth(mp3, hps)
+            inputs = torch.tensor(mp3[:881920]).view(1, -1, 1).to(device)
 
-        csv['client'].append(client_name)
-        csv['media_id'].append(mp3_metadata['media_id'])
-        csv['external_id'].append(mp3_metadata['external_id'])
-        csv['s3_key'].append(mp3_metadata['s3_key'])
-        for k, v in _metrics.items():
-            csv[k].append(float(v.squeeze().cpu().numpy()))
+            mp3_spec = spec(inputs.squeeze().cpu(), hps).numpy()
+            # save_spec_plot(mp3_spec, os.path.join(output_folder, client_name, 'spec', filename.split('.')[0] + '.png'),
+            #                title=filename.split('.')[0])
 
-        pd.DataFrame(csv).to_csv(os.path.join(output_folder, 'metrics.csv'))
+            inputs = audio_preprocess(inputs, hps)
+            x_outs, loss, _metrics = vqvae(inputs, **forw_kwargs, return_all_x_outs=True)  # x_outs with top level first
 
+            # print("Loss: {}".format(loss))
+            # print("Metrics:", _metrics)
+
+            out_specs = []
+            for i, x_out in enumerate(reversed(x_outs)):  # level 0 (bottom) first
+                x_out_np = x_out.cpu().squeeze().numpy()
+                librosa.output.write_wav("{}/{}_recon{}.wav".format(os.path.join(output_folder, client_name, 'audio'),
+                                                                  filename.split('.')[0], i),
+                                         x_out_np, sr=44100)
+                x_out_spec = spec(x_out.squeeze().cpu(), hps).numpy()
+                out_specs.append(x_out_spec)
+
+            save_spec_plot([mp3_spec] + out_specs, os.path.join(output_folder, client_name, 'spec', filename.split('.')[0] + '.png'),
+                           title=filename.split('.')[0])
+
+            csv['client'].append(client_name)
+            csv['media_id'].append(mp3_metadata['media_id'])
+            csv['external_id'].append(mp3_metadata['external_id'])
+            csv['s3_key'].append(mp3_metadata['s3_key'])
+            for k, v in _metrics.items():
+                csv[k].append(float(v.squeeze().cpu().numpy()))
+
+            pd.DataFrame(csv).to_csv(os.path.join(output_folder, 'metrics.csv'))
+
+
+def compute_codes(vqvae, hps, output_folder):
+    json_path = '/home/kevin/feedforward/sandbox/mp3s_for_jukebox_encodings.json'
+    mp3_dict = load_json(json_path)
+
+    hps.argv = " ".join(sys.argv)
+    hps.bs_sample = hps.nworkers = hps.bs = 1
+
+    for client_name in mp3_dict:
+        level_0_codes, level_1_codes, level_2_codes = [], [], []
+        successful_mp3s = []
+
+        if not os.path.exists(os.path.join(output_folder, client_name)):
+            os.makedirs(os.path.join(output_folder, client_name))
+
+        for mp3_metadata in mp3_dict[client_name]:  # 'external_id', 'media_id', 'num_samples', 's3_key'
+            s3_key = mp3_metadata['s3_key']
+            filename = s3_key.split('/')[-1]
+            mp3_path = os.path.join(audio_mp3s_folder, s3_key)
+            try:
+                mp3, _ = librosa.core.load(mp3_path, sr=44100)
+
+                hps.bandwidth = get_bandwidth(mp3, hps)
+
+                # Rough test showed that 5M samples fits OK into the GPU, but 5.5M crashes
+                bs_chunks = int(np.ceil(mp3.size / 5e6))
+                inputs = torch.tensor(mp3).view(1, -1, 1).to(device)
+
+                # inputs = audio_preprocess(inputs, hps)  # This doesn't do anything anyway
+                codes0, codes1, codes2 = vqvae.encode(inputs, bs_chunks=bs_chunks)
+
+                # Success
+                level_0_codes.append(codes0.detach().cpu().numpy().tolist())
+                level_1_codes.append(codes1.detach().cpu().numpy().tolist())
+                level_2_codes.append(codes2.detach().cpu().numpy().tolist())
+                successful_mp3s.append(mp3_metadata)
+
+                save_as_json(level_0_codes, os.path.join(output_folder, client_name, 'level_0_codes.json'))
+                save_as_json(level_1_codes, os.path.join(output_folder, client_name, 'level_1_codes.json'))
+                save_as_json(level_2_codes, os.path.join(output_folder, client_name, 'level_2_codes.json'))
+
+                save_as_json(successful_mp3s, os.path.join(output_folder, client_name, 'successful_mp3s.json'))
+            except Exception as e:
+                continue
+
+
+
+# compute_metrics(vqvae, hps, output_folder)
+compute_codes(vqvae, hps, output_folder)
